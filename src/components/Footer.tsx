3789b7f0d07b0ae7e8e3ef6d9f1df4a1dd6550c0
Mogakamo <mogaka.amo254@gmail.com>
2022-10-30 16:49:48 +0300

    [ui]: finished with the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,17 @@ import I4G from "../assets/I4G.png";
 const Footer = () => {
   return (
     <div className="py-[48px] flex md:flex-row flex-col items-start justify-start md:items-center w-full md:justify-between">
-      <img src={zuri} alt="Zuri Team" />
+      <a href="https://training.zuri.team/">
+        <img src={zuri} alt="Zuri Team" />
+      </a>
+
       <p className="text-gray-500 text-[16px]">
         HNG Internship 9 Frontend Task
       </p>
-      <img src={I4G} alt="Ingresive 4 Good" />
+      
+      <a href="https://ingressive.org/">
+        <img src={I4G} alt="Ingresive 4 Good" />
+      </a>
     </div>
   );
 };
