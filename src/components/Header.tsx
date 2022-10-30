09c0ce8443edabb91d83335ce38bc7074b089f22
Mogakamo <mogaka.amo254@gmail.com>
2022-10-30 18:10:26 +0300

    [fix]: added the share btn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import profile_img from "../assets/amos.jpg";
+import share from "../assets/icons/share.png";
 
 const Header = () => {
   return (
     <div className="flex flex-col items-center">
+      <button className="ml-64" title="Share link">
+        <img src={share} alt="Share btn" className="ml-96 h-10" />
+      </button>
+
       <img
         src={profile_img}
         alt="Profile picture"
         id="profile__img"
         className="h-[88px] w-[88px] rounded-full"
       />
+
       {/* Twitter username */}
       <a
         href="https://twitter.com/m_ogakadev"
