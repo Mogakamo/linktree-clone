2a790cb78ace5e64a8e745d3fd8edf8a0943a2d6
Mogakamo <mogaka.amo254@gmail.com>
2022-11-05 20:35:50 +0300

    [ui]: Contact page is done

--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -1,24 +1,82 @@
 import React from "react";
+import Footer from "../components/Footer";
 
 const Contact = () => {
   return (
-    <div className="mx-48 my-28 h-screen">
-      <h1 className="text-">Contact Me</h1>
-      <p>Hi there, contact me to ask me about anything you have in mind.</p>
-
-      <div className="grid grid-cols-2 gap-10">
-        <div className="flex flex-col">
-          <label htmlFor="f_name">First name</label>
+    <div className="md:mx-48 md:my-28 my-10 px-5 h-auto space-y-5">
+      <h1 className="text-[36px] font-semibold">Contact Me</h1>
+      <p className="text-[20px] text-gray-600 leading-[30px]">
+        Hi there, contact me to ask me about anything you have in mind.
+      </p>
+      <div className="md:grid grid-cols-2 gap-10 mt-5 ">
+        <div className="flex flex-col space-y-2">
+          <label
+            htmlFor="f_name"
+            className="text-[14px] text-gray-700 font-medium"
+          >
+            First name
+          </label>
           <input
             type="text"
             placeholder="Enter your first name"
-            className="border px-5 py-2 rounded-md shadow-md"
+            className="border-2 px-5 py-2 rounded-md shadow-md outline-none"
           />
         </div>
-        <div>
-          <label htmlFor="f_name">First name</label>
-          <input type="text" placeholder="Enter your first name" />
+        <div className="flex flex-col space-y-2 mt-3">
+          <label
+            htmlFor="f_name"
+            className="text-[14px] text-gray-700 font-medium"
+          >
+            Last name
+          </label>
+          <input
+            type="text"
+            placeholder="Enter your last name"
+            className="border-2 px-5 py-2 rounded-md shadow-md  outline-none"
+          />
         </div>
+      </div>
+      <div className=" space-y-2">
+        <label
+          htmlFor="email"
+          className="text-[14px] text-gray-700 font-medium"
+        >
+          Email
+        </label>
+        <input
+          type="email"
+          placeholder="yourname@gmail.com"
+          className="w-full border-2 px-5 py-2 rounded-md  outline-none shadow-md"
+        />
+      </div>
+      <div className="flex flex-col space-y-3">
+        <label
+          htmlFor="message"
+          className="text-[14px] text-gray-700 font-medium"
+        >
+          Message
+        </label>
+        <textarea
+          placeholder="Send me a message and I'll reply you as soon as possible..."
+          className="border-2 px-5 py-2 rounded-md shadow-md outline-none h-[132px]"
+        ></textarea>
+      </div>
+      <div className="space-x-1 flex items-center">
+        <input
+          type="checkbox"
+          id=""
+          placeholder=""
+          className="h-[20px] w-[20px] rounded-lg"
+        />
+        <span className="text-[16px] text-gray-600">
+          You agree to providing your data to Amos Nyaburi who may contact you.
+        </span>
+      </div>
+      <button className="w-full bg-blue-600 px-[12px] text-white rounded-md py-[20px]">
+        Send message
+      </button>
+      <div className="border-t-2 ">
+        <Footer />
       </div>
     </div>
   );
